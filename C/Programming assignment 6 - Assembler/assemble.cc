b6a9ce7220aabe0c3941f188ca4f8e8bd90d491a
Alex Wohlbruck <alexwohlbruck@gmail.com>
2019-11-22 11:16:43 -0500

    Start getAdd

--- C/Programming assignment 6 - Assembler/assemble.cc
+++ C/Programming assignment 6 - Assembler/assemble.cc
@@ -4,16 +4,13 @@
 #define LIMIT 10000
 #define LINE_SIZE 128
 
-//For part 1
+// For part 1
 void assemble(char[]);
 int findOrigin(FILE*);
 int firstPass(FILE*, int[], int);
 void printLabels(int[]);
 
-void trim(char*);
-char isspace(unsigned char c);
-void strlwr(char *s);
-//For part 2
+// For part 2
 int getAdd(char[]);
 int getAnd(char[]);
 int getTrap(char[]);
@@ -26,28 +23,33 @@ int getBr(char[], int[], int);
 int getIt(int, char[]);
 int secondPass(FILE*, int[], int);
 
-void assemble(char filename[])
-{
+// Helper functions
+void trim(char*);
+char isspace(unsigned char c);
+void strlwr(char *s);
+int strequals(char *s1, char *s2);
+void removeChunk(char *line, int num);
+char *chartobin(char num, int length);
+char *inttobin(int num, int length);
+
+void assemble(char filename[]) {
 	//Open the file for reading
   FILE *infile = fopen( filename , "r" );
     
-	if (infile != NULL) 
-	{    
+	if (infile != NULL)  {    
 		//Create labels array and set all elements to -1.  
 		int labels[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
 		
 		int lc = findOrigin(infile);
 		
-		if (lc > -1)
-		{
+		if (lc > -1) {
 			//Read in label values
-			if (!firstPass(infile, labels, lc))
-			{
+			if (!firstPass(infile, labels, lc)) {
 				//Show the labels.
-				printLabels(labels);
+				// printLabels(labels);
 				
 				//The following is for part 2
-				//secondPass(infile, labels, lc);
+				secondPass(infile, labels, lc);
 			}
 		}
 		
@@ -55,14 +57,12 @@ void assemble(char filename[])
 		fclose(infile);
 		
 	} 
-	else 
-	{	
+	else {	
 		printf("Can't open input file.\n");		
 	}
 }
 
-int findOrigin(FILE *infile)
-{
+int findOrigin(FILE *infile) {
 	//Each trip through the while loop will read the next line of infile
 	//into the line char array as a null terminated string.
 	char line[LINE_SIZE];
@@ -79,8 +79,7 @@ int findOrigin(FILE *infile)
 	char c = 0;
 	
 	//Read lines until EOF reached or LIMIT lines read.  Limit prevent infinite loop.
-	while (!done && lineCount < LIMIT && fscanf(infile, "%[^\n]s", line) != EOF)
-	{
+	while (!done && lineCount < LIMIT && fscanf(infile, "%[^\n]s", line) != EOF) {
 		lineCount++;
 		
 		fscanf(infile, "%c", &c);  //Get rid of extra newline.
@@ -148,8 +147,7 @@ int findOrigin(FILE *infile)
 	return -1;
 }
 
-int firstPass(FILE *infile, int labels[], int lc)
-{
+int firstPass(FILE *infile, int labels[], int lc) {
 	//Create a while loop similar to findOrigin.
 	//You can rewind if you want to but you shouldn't have to.
 
@@ -169,8 +167,7 @@ int firstPass(FILE *infile, int labels[], int lc)
 	char c = 0;
 	
 	//Read lines until EOF reached or LIMIT lines read.  Limit prevent infinite loop.
-	while (!done && lineCount < LIMIT && fscanf(infile, "%[^\n]s", line) != EOF)
-	{
+	while (!done && lineCount < LIMIT && fscanf(infile, "%[^\n]s", line) != EOF) {
 		lineCount++;
 		
 		fscanf(infile, "%c", &c);  //Get rid of extra newline.
@@ -182,23 +179,24 @@ int firstPass(FILE *infile, int labels[], int lc)
 
 		int lineIsEmpty = strlen(line) == 0;
 		int lineIsComment = line[0] == ';';
-		int lineIsOrigin = strncmp(line, ".orig", 5) == 0;
-		int lineIsEnd = strncmp(line, ".end", 4) == 0;
+		int lineIsOrigin = strequals(line, ".orig");
+		int lineIsEnd = strequals(line, ".end");
 		int lineIsLabel = line[0] == 'l' && line[1] >= '0' && line[1] <= '9' && strlen(line) == 2;
 		int lineIsFill = strstr(line, ".fill") != NULL;
 
-		int lineIsAdd = strncmp(line, "add", 3) == 0;
-		int lineIsAnd = strncmp(line, "and", 3) == 0;
-		int lineIsNot = strncmp(line, "not", 3) == 0;
-		int lineIsLd = strncmp(line, "ld", 2) == 0;
-		int lineIsLdr = strncmp(line, "ldr", 3) == 0;
-		int lineIsSt = strncmp(line, "st", 2) == 0;
-		int lineIsSTR = strncmp(line, "str", 3) == 0;
-		int lineIsBR = strncmp(line, "br", 2) == 0;
-		int lineIsTrap = strncmp(line, "trap", 4) == 0;
+		int lineIsAdd = strequals(line, "add");
+		int lineIsAnd = strequals(line, "and");
+		int lineIsNot = strequals(line, "not");
+		int lineIsLd = strequals(line, "ld");
+		int lineIsLdr = strequals(line, "ldr");
+		int lineIsSt = strequals(line, "st");
+		int lineIsStr = strequals(line, "str");
+		int lineIsBr = strequals(line, "br");
+		int lineIsTrap = strequals(line, "trap");
+		int lineIsInstruction = lineIsAdd || lineIsAnd || lineIsNot || lineIsLd || lineIsLdr || lineIsSt || lineIsStr || lineIsBr || lineIsTrap;
 
 
-		// If the line is a comment, a blank line or the .orig directive, don�t do anything.
+		// If the line is a comment, a blank line or the .orig directive, don't do anything.
 		if (!lineIsEmpty && !lineIsComment && !lineIsOrigin) {
 			
 			int labelNum = 0;
@@ -218,7 +216,7 @@ int firstPass(FILE *infile, int labels[], int lc)
 
 			// If the line is one of the allowed instructions (ADD, AND, NOT, LD, LDR, ST, STR, BR, and TRAP) increment the lc.
 			// I am so sorry that I did this
-			if (lineIsAdd || lineIsAnd || lineIsNot || lineIsLd || lineIsLdr || lineIsSt || lineIsSTR || lineIsBR || lineIsTrap) {
+			if (lineIsInstruction) {
 				lc++;
 			}
 
@@ -246,6 +244,172 @@ int firstPass(FILE *infile, int labels[], int lc)
 		return -1;
 	}
 }
+
+int secondPass(FILE *infile, int labels[], int lc) {
+	rewind(infile);
+
+	//Create a while loop similar to findOrigin.
+	//You can rewind if you want to but you shouldn't have to.
+
+	//Each trip through the while loop will read the next line of infile
+	//into the line char array as a null terminated string.
+	char line[LINE_SIZE];
+	
+	//The variable lineCount keeps track of how many lines have been read.
+	//It is used to guard against infinite loops.  Don't remove anything
+	//that has to do with linecount.
+	int lineCount = 0; //
+	
+	//For getting out of the while loop.
+	int done = 0;
+	
+	//For getting rid of the trailing newline
+	char c = 0;
+	
+	//Read lines until EOF reached or LIMIT lines read.  Limit prevent infinite loop.
+	while (!done && lineCount < LIMIT && fscanf(infile, "%[^\n]s", line) != EOF) {
+		lineCount++;
+		
+		fscanf(infile, "%c", &c);  //Get rid of extra newline.
+		
+		//Read a line.
+
+		trim(line);
+		strlwr(line);
+
+		int lineIsEmpty = strlen(line) == 0;
+		int lineIsComment = line[0] == ';';
+		int lineIsOrigin = strequals(line, ".orig");
+		int lineIsEnd = strequals(line, ".end");
+		int lineIsLabel = line[0] == 'l' && line[1] >= '0' && line[1] <= '9' && strlen(line) == 2;
+		int lineIsFill = strstr(line, ".fill") != NULL;
+
+		int lineIsAdd = strequals(line, "add");
+		int lineIsAnd = strequals(line, "and");
+		int lineIsNot = strequals(line, "not");
+		int lineIsLd = strequals(line, "ld");
+		int lineIsLdr = strequals(line, "ldr");
+		int lineIsSt = strequals(line, "st");
+		int lineIsStr = strequals(line, "str");
+		int lineIsBr = strequals(line, "br");
+		int lineIsTrap = strequals(line, "trap");
+		int lineIsInstruction = lineIsAdd || lineIsAnd || lineIsNot || lineIsLd || lineIsLdr || lineIsSt || lineIsStr || lineIsBr || lineIsTrap;
+
+		// Logic here
+
+		if (!lineIsEmpty && !lineIsComment && !lineIsOrigin && !lineIsLabel) {
+			// Ignore line
+		}
+		else if (lineIsFill) {
+			lc++;
+			printf("0000\n");
+		}
+		else if (lineIsEnd) {
+			return 0;
+		}
+		
+		if (lineIsInstruction) {
+			lc++;
+			
+			// Call appropriate method to deal with the instruction
+			if (lineIsAdd) {
+				getAdd(line);
+			} else if (lineIsAnd) {
+				getAnd(line);
+			} else if (lineIsNot) {
+				getNot(line);
+			} else if (lineIsLd) {
+				getLd(line, labels, lc);
+			} else if (lineIsLdr) {
+				getLdr(line);
+			} else if (lineIsSt) {
+				getSt(line, labels, lc);
+			} else if (lineIsStr) {
+				getStr(line);
+			} else if (lineIsBr) {
+				getBr(line, labels, lc);
+			} else if (lineIsTrap) {
+				getTrap(line);
+			}
+		}
+	}
+}
+
+int getAdd(char line[]) {
+	// ADD R1, R2, R3
+	// 0001 DR(3) SR1(3) 0 00 SR2(3)
+	// 0001 DR(3) SR1(3) 1 imm5(5)
+	char output[] = "0001";
+
+	removeChunk(line, 3); // Remove "ADD"
+
+	char DR = line[1];
+	strcpy(output, chartobin(DR)); // Append DR to output
+	
+	removeChunk(line, 1); // Remove ","
+
+	int SR1 = line[1];
+	strcpy(output, chartobin(SR1)); // Append SR1 to output
+
+	removeChunk(line, 2); // Remove "R{x}"
+	removeChunk(line, 1); // Remove ","
+
+	if (line[0] == 'R') {
+		// Normal mode 
+
+		int SR2 = line[1];
+		strcpy(output, "000"); // Append placeholder to output
+		strcpy(output, chartobin(SR2) + 1); // Append SR2 to output
+	}
+	else if (line[0] == '#') {
+		// Immediate mode
+
+		line += 1; // Remove "#"
+		
+		int num = 0;
+		sscanf(line, "%d", &num); // Scan number into int
+		
+		strcpy(output, "1");
+		strcpy(output, inttobin(num)); // Append immediate val to output
+	}
+}
+
+int getAnd(char line[]) {
+
+}
+
+int getTrap(char line[]) {
+
+}
+
+int getNot(char line[]) {
+
+}
+
+int getLd(char line[], int[], int) {
+
+}
+
+int getLdr(char line[]) {
+
+}
+
+int getSt(char line[], int[], int) {
+
+}
+
+int getStr(char line[]) {
+
+}
+
+int getBr(char line[], int[], int) {
+
+}
+
+/* int getIt(int, char[]) {
+
+} */
+
 
 void printLabels(int labels[]) {
 	printf("labels = {");
@@ -278,8 +442,7 @@ void trim(char *str) {
 }
 
 // Determine whether a char is whitespace
-char isspace(unsigned char c) 
-{
+char isspace(unsigned char c) {
   if ( c == ' '
     || c == '\f'
     || c == '\n'
@@ -292,8 +455,7 @@ char isspace(unsigned char c)
 }
 
 // Transform string to lowercase
-void strlwr(char *s)
-{
+void strlwr(char *s) {
     int i=0;
     while(s[i]!='\0')
     {
@@ -302,4 +464,50 @@ void strlwr(char *s)
         }
         ++i;
     }
+}
+
+// Determine if a string is equal to another based on length of the second string
+int strequals(char *s1, char *s2) {
+	return strncmp(s1, s2, strlen(s2)) == 0;
+}
+
+// Remove chunk of text from string and trim it
+void removeChunk(char *line, int num) {
+	line += num;
+	trim(line);
+}
+
+
+char *inttobin(int num, int length) {
+	
+}
+
+// Convert ascii-represented integer character to 4 digit binary string
+char *chartobin(char num, int length) {
+	switch (num) {
+		case '0':
+			return "0000";
+		case '1':
+			return "0001";
+		case '2':
+			return "0010";
+		case '3':
+			return "0011";
+		case '4':
+			return "0100";
+		case '5':
+			return "0101";
+		case '6':
+			return "0110";
+		case '7':
+			return "0111";
+		case '8':
+			return "1000";
+		case '9':
+			return "1001";
+	}
+}
+
+int bintoint(const char *s) {
+  return (int) strtol(s, NULL, 2);
 }
